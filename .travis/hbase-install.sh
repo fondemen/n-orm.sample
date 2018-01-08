1e3e6b977bdde16da89ea1ec1c0db6414eebdb32
Frédéric Fondement <frederic.fondement@uha.fr>
2018-01-09 00:40:08 +0100

    Travis integration

--- .travis/hbase-install.sh
+++ .travis/hbase-install.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
 VER=1.4.0
+DST=/usr/lib
 wget -O hbase-bin.tar.gz http://www-eu.apache.org/dist/hbase/$VER/hbase-$VER-bin.tar.gz
 tar xzf hbase-bin.tar.gz
 rm -f hbase-$VER/conf/hbase-site.xml
 mv .travis/hbase-site.xml hbase-$VER/conf
-sudo hbase-$VER/bin/start-hbase.sh
+sudo mkdir -p $DST
+sudo mv hbase-$VER $DST/hbase
+sudo $DST/hbase/bin/start-hbase.sh
